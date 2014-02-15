1bda0726dae76e05376c7c8d7a67523db7843e38
suresh <suresh@suresh-desktop.(none)>
2014-02-15 14:50:46 +0530

    ability to delete

--- src/com/example/todo/TodoActivity.java
+++ src/com/example/todo/TodoActivity.java
@@ -5,7 +5,11 @@ import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 public class TodoActivity extends Activity {
@@ -23,6 +27,7 @@ public class TodoActivity extends Activity {
         lvItems.setAdapter(itemsAdapter);
         items.add("First Item");
         items.add("Second Item");
+        setupListViewListener();
     }
 
 
@@ -31,6 +36,22 @@ public class TodoActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.todo, menu);
         return true;
+    }
+    
+    public void addTodoItem(View v){
+    	EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+    	itemsAdapter.add(etNewItem.getText().toString());
+    	etNewItem.setText("");
+    	
+    }
+    public void setupListViewListener(){
+    	lvItems.setOnItemLongClickListener(new OnItemLongClickListener(){
+    		public boolean onItemLongClick(AdapterView<?> parent,View view, int position, long rowId){
+    			items.remove(position);
+    			itemsAdapter.notifyDataSetChanged();
+    			return true;
+    		}
+    	});
     }
     
 }
